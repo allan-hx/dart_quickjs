81c3f8f4bb11dc1174fe78711bcd21e1b6edf7ae
Allan <allan@mobox.io>
2023-03-13 21:28:41 +0800

    Uploaded code

---
+++ ios/cxx/ffi.cpp
@@ -0,0 +1,181 @@
+#include "quickjs.h"
+#include <string.h>
+#include "ffi.h"
+
+// 通道
+static JSValue _channel(JSContext *ctx, JSValueConst this_val, int32_t argc, JSValueConst *argv, int32_t magic, JSValue *method) {
+  JSRuntime *runtime = JS_GetRuntime(ctx);
+  CHANNEL *channel = (CHANNEL *)JS_GetRuntimeOpaque(runtime);
+  return *channel(ctx, JS_ToCString(ctx, *method), argc, argv);
+}
+
+// 模块加载
+static JSModuleDef *_moduleLoader(JSContext *ctx, const char *module_name, void *opaque) {
+  JSRuntime *runtime = JS_GetRuntime(ctx);
+  CHANNEL *channel = (CHANNEL *)JS_GetRuntimeOpaque(runtime);
+  const char *str = (char *)channel(ctx, "module_loader", 1, new JSValue(JS_NewString(ctx, module_name)));
+
+  if (str == 0) {
+    return NULL;
+  }
+
+  JSValue value = JS_Eval(ctx, str, strlen(str), module_name, JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
+
+  if (JS_IsException(value)) {
+    return NULL;
+  }
+
+  JSModuleDef *m = (JSModuleDef *)JS_VALUE_GET_PTR(value);
+  JS_FreeValue(ctx, value);
+  return m;
+}
+
+extern "C" {
+  DART_EXPORT void SetChannel(JSRuntime *runtime, CHANNEL channel) {
+    JS_SetRuntimeOpaque(runtime, (void *)channel);
+  }
+
+  DART_EXPORT void EnableModuleLoader(JSRuntime *runtime) {
+    JS_SetModuleLoaderFunc(runtime, NULL, _moduleLoader, NULL);
+  }
+
+  DART_EXPORT JSValue *GetGlobalObject(JSContext *ctx) {
+    return new JSValue(JS_GetGlobalObject(ctx));
+  }
+
+  DART_EXPORT JSValue *EvaluateJavaScript(JSContext *ctx, const char *script, const char *fileName, int32_t flags) {
+    JSRuntime *runtime = JS_GetRuntime(ctx);
+    JS_UpdateStackTop(runtime);
+    JSValue value = JS_Eval(ctx, script, strlen(script), fileName, flags);
+    return new JSValue(value);
+  }
+
+  DART_EXPORT uint32_t JSValueSizeOf() {
+    return sizeof(JSValue);
+  }
+
+  DART_EXPORT void SetValueAt(JSValue *data, uint32_t index, JSValue *value) {
+    data[index] = *value;
+  }
+
+  DART_EXPORT JSValue *GetException(JSContext *ctx) {
+    return new JSValue(JS_GetException(ctx));
+  }
+
+  DART_EXPORT int32_t ExecutePendingJob(JSRuntime *runtime) {
+    JSContext *ctx;
+    int ret = JS_ExecutePendingJob(runtime, &ctx);
+    return ret;
+  }
+
+  DART_EXPORT JSValue *NewObject(JSContext  *ctx) {
+    return new JSValue(JS_NewObject(ctx));
+  }
+
+  DART_EXPORT JSValue *NewString(JSContext *ctx, const char *data) {
+    return new JSValue(JS_NewString(ctx, data));
+  }
+
+  DART_EXPORT JSValue *NewInt64(JSContext *ctx, int64_t value) {
+    return new JSValue(JS_NewInt64(ctx, value));
+  }
+
+  DART_EXPORT JSValue *NewFloat64(JSContext *ctx, double value) {
+    return new JSValue(JS_NewFloat64(ctx, value));
+  }
+
+  DART_EXPORT JSValue *NewBool(JSContext *ctx, int32_t value) {
+    return new JSValue(JS_NewBool(ctx, value));
+  }
+
+  DART_EXPORT JSValue *NewArray(JSContext *ctx) {
+    return new JSValue(JS_NewArray(ctx));
+  }
+
+  DART_EXPORT JSValue *NewPromiseCapability(JSContext *ctx, JSValue *resolving_funcs) {
+    return new JSValue(JS_NewPromiseCapability(ctx, resolving_funcs));
+  }
+
+  DART_EXPORT JSValue *NewCFunctionData(JSContext *ctx, const char *symbol) {
+    JSValue value = JS_NewCFunctionData(ctx, _channel, 0, 0, 1, new JSValue(JS_NewString(ctx, symbol)));
+    return new JSValue(value);
+  }
+
+  DART_EXPORT JSValue *NewNull() {
+    return new JSValue(JS_NULL);
+  }
+
+  DART_EXPORT JSValue *NewUndefined() {
+    return new JSValue(JS_UNDEFINED);
+  }
+
+  DART_EXPORT int32_t SetPropertyStr(JSContext *ctx, JSValueConst *obj, const char *key, JSValue *value) {
+    return JS_SetPropertyStr(ctx, *obj, key, *value);
+  }
+
+  DART_EXPORT int32_t SetProperty(JSContext *ctx, JSValueConst *obj, JSValueConst *key, JSValueConst *value, int flags) {
+    JSAtom atom = JS_ValueToAtom(ctx, *key);
+    int32_t ret = JS_SetPropertyInternal(ctx, *obj, atom, *value, flags);
+    JS_FreeAtom(ctx, atom);
+    return ret;
+  }
+
+  DART_EXPORT int DefinePropertyValueUint32(JSContext *ctx, JSValueConst *obj, uint32_t index, JSValue *value, int32_t flags) {
+    return JS_DefinePropertyValueUint32(ctx, *obj, index, *value, flags);
+  }
+
+  JSValue *GetPropertyStr(JSContext *ctx, JSValueConst *obj, const char *prop) {
+    return new JSValue(JS_GetPropertyStr(ctx, *obj, prop));
+  }
+
+  DART_EXPORT JSValue *GetProperty(JSContext *ctx, JSValueConst *obj, JSValueConst *key) {
+    JSAtom atom = JS_ValueToAtom(ctx, *key);
+    JSValue *value = new JSValue(JS_GetProperty(ctx, *obj, atom));
+    JS_FreeAtom(ctx, atom);
+    return value;
+  }
+
+  DART_EXPORT JSValue *JSDupValue(JSContext *ctx, JSValueConst *value) {
+    return new JSValue(JS_DupValue(ctx, *value));
+  }
+
+  DART_EXPORT void JSFreeValue(JSContext *ctx, JSValue *value) {
+    JS_FreeValue(ctx, *value);
+  }
+
+  DART_EXPORT const char *JSToCString(JSContext *ctx, JSValueConst *value) {
+    return JS_ToCString(ctx, *value);
+  }
+
+  DART_EXPORT int64_t JSToInt64(JSContext *ctx, JSValueConst *value) {
+    int64_t data;
+    JS_ToInt64(ctx, &data, *value);
+    return data;
+  }
+
+  DART_EXPORT double JSToFloat64(JSContext *ctx, JSValueConst *value) {
+    double data;
+    JS_ToFloat64(ctx, &data, *value);
+    return data;
+  }
+
+  DART_EXPORT int32_t JSToBool(JSContext *ctx, JSValueConst *value) {
+    return JS_ToBool(ctx, *value);
+  }
+
+  DART_EXPORT JSValue *CallFuncton(JSContext *ctx, JSValueConst *func_obj, JSValueConst *this_obj, int32_t argc, JSValueConst *argv) {
+    return new JSValue(JS_Call(ctx, *func_obj, *this_obj, argc, argv));
+  }
+
+  DART_EXPORT int32_t IsArray(JSContext *ctx, JSValueConst *value) {
+    return JS_IsArray(ctx, *value);
+  }
+
+  DART_EXPORT int32_t IsFunction(JSContext *ctx, JSValueConst *value) {
+    return JS_IsFunction(ctx, *value);
+  }
+
+  DART_EXPORT int32_t IsPromise(JSContext *ctx, JSValueConst *value) {
+    return JS_IsPromise(ctx, *value);
+  }
+}
---
+++ ios/cxx/ffi.h
@@ -0,0 +1,112 @@
+#include "quickjs.h"
+#include <string.h>
+
+#ifdef _MSC_VER
+#define DART_EXPORT __declspec(dllexport)
+#else
+#define DART_EXPORT __attribute__((visibility("default"))) __attribute__((used))
+#endif
+
+// 通道
+typedef JSValue *CHANNEL(JSContext *ctx, const char *symbol, int32_t argc, JSValueConst *argv);
+
+extern "C" {
+  // 设置通道
+  DART_EXPORT void SetChannel(JSRuntime *runtime, CHANNEL channel);
+
+  // 启用模块加载
+  DART_EXPORT void EnableModuleLoader(JSRuntime *runtime);
+
+  // 获取全局对象
+  DART_EXPORT JSValue *GetGlobalObject(JSContext *ctx);
+
+  // 执行js代码
+  DART_EXPORT JSValue *EvaluateJavaScript(JSContext *ctx, const char *script, const char *fileName, int32_t flags);
+
+  // 获取js value的大小
+  DART_EXPORT uint32_t JSValueSizeOf();
+
+  // 根据下标设置value
+  DART_EXPORT void SetValueAt(JSValue *data, uint32_t index, JSValue *value);
+
+  // 获取异常
+  DART_EXPORT JSValue *GetException(JSContext *ctx);
+
+  // 执行挂起任务
+  DART_EXPORT int32_t ExecutePendingJob(JSRuntime *runtime);
+
+  // 创建对象
+  DART_EXPORT JSValue *NewObject(JSContext  *ctx);
+
+  // 创建字符串
+  DART_EXPORT JSValue *NewString(JSContext *ctx, const char *data);
+
+  // 创建int64
+  DART_EXPORT JSValue *NewInt64(JSContext *ctx, int64_t value);
+
+  // 创建float64
+  DART_EXPORT JSValue *NewFloat64(JSContext *ctx, double value);
+
+  // 创建bool
+  DART_EXPORT JSValue *NewBool(JSContext *ctx, int32_t value);
+
+  // 创建数组
+  DART_EXPORT JSValue *NewArray(JSContext *ctx);
+
+  // 创建promise
+  DART_EXPORT JSValue *NewPromiseCapability(JSContext *ctx, JSValue *resolving_funcs);
+
+  // 创建js方法
+  DART_EXPORT JSValue *NewCFunctionData(JSContext *ctx, const char *symbol);
+
+  // 创建null
+  DART_EXPORT JSValue *NewNull();
+
+  // 创建undefined
+  DART_EXPORT JSValue *NewUndefined();
+
+  // 添加属性 - key为string类型
+  DART_EXPORT int32_t SetPropertyStr(JSContext *ctx, JSValueConst *obj, const char *key, JSValue *value);
+
+  // 添加属性 - key为js value
+  DART_EXPORT int32_t SetProperty(JSContext *ctx, JSValueConst *obj, JSValueConst *key, JSValueConst *value, int flags);
+
+  // 添加属性 - key为uint32
+  DART_EXPORT int DefinePropertyValueUint32(JSContext *ctx, JSValueConst *obj, uint32_t index, JSValue *value, int32_t flags);
+
+  // 读取属性 - key为string类型
+  JSValue *GetPropertyStr(JSContext *ctx, JSValueConst *obj, const char *prop);
+
+  // 读取属性 - key为js value
+  DART_EXPORT JSValue *GetProperty(JSContext *ctx, JSValueConst *obj, JSValueConst *key);
+
+  // 增加引用标记
+  DART_EXPORT JSValue *JSDupValue(JSContext *ctx, JSValueConst *value);
+
+  // 释放js value
+  DART_EXPORT void JSFreeValue(JSContext *ctx, JSValue *value);
+
+  // js value转string
+  DART_EXPORT const char *JSToCString(JSContext *ctx, JSValueConst *value);
+
+  // 转int 64
+  DART_EXPORT int64_t JSToInt64(JSContext *ctx, JSValueConst *value);
+
+  // 转Float 64
+  DART_EXPORT double JSToFloat64(JSContext *ctx, JSValueConst *value);
+
+  // 转布尔值
+  DART_EXPORT int32_t JSToBool(JSContext *ctx, JSValueConst *value);
+
+  // 调用js方法
+  DART_EXPORT JSValue *CallFuncton(JSContext *ctx, JSValueConst *func_obj, JSValueConst *this_obj, int32_t argc, JSValueConst *argv);
+
+  // 是否是数组
+  DART_EXPORT int32_t IsArray(JSContext *ctx, JSValueConst *value);
+
+  // 是否是函数
+  DART_EXPORT int32_t IsFunction(JSContext *ctx, JSValueConst *value);
+
+  // 是否是promise
+  DART_EXPORT int32_t IsPromise(JSContext *ctx, JSValueConst *value);
+}
